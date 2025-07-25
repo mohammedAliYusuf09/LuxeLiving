59bc39b04b8228d9b2e4477b66533b9804ce7af5
Yusuf Ali <isaihmed09@gmail.com>
2025-07-25 20:35:12 +0600

    Some Laout Folt fixed

--- frontend/src/components/LeftNavbar.tsx
+++ frontend/src/components/LeftNavbar.tsx
@@ -1,14 +1,40 @@
 import { NavLink } from "react-router-dom";
+import { LuHouse } from "react-icons/lu";
+import { FaBlog } from "react-icons/fa6";
+import { FiMessageSquare } from "react-icons/fi";
+import { CiSettings } from "react-icons/ci";
 
 const LeftNavbar = () => {
   return (
     <div className="w-64 h-screen bg-[#171821] text-white p-4 border-r border-[#2C2D33]">
       {/* <h2 className="text-xl font-bold mb-6">My App</h2> */}
       <nav className="flex flex-col gap-4">
-        <NavLink to="/propertys" className="hover:text-blue-400 p-3 rounded-md">Propertys</NavLink>
-        <NavLink to="/blogs" className="hover:text-blue-400 p-3 rounded-md">Blogs</NavLink>
-        <NavLink to="/messages" className="hover:text-blue-400 p-3 rounded-md">Messages</NavLink>
-        <NavLink to="/settings" className="hover:text-blue-400 p-3 rounded-md">Settings</NavLink>
+        <NavLink to="/propertys" className="hover:text-blue-400 p-3 rounded-md text-gray-200">
+          <span className="flex items-center gap-2">
+          <LuHouse />
+          Propertys
+          </span>
+
+
+        </NavLink>
+        <NavLink to="/blogs" className="hover:text-blue-400 p-3 rounded-md text-gray-200">
+        <span className="flex items-center gap-2">
+          <FaBlog />
+          Blogs
+          </span>
+        </NavLink>
+        <NavLink to="/messages" className="hover:text-blue-400 p-3 rounded-md text-gray-200">
+          <span className="flex items-center gap-2">
+          <FiMessageSquare />
+          Messages
+          </span>
+        </NavLink>
+        <NavLink to="/settings" className="hover:text-blue-400 p-3 rounded-md text-gray-200">
+          <span className="flex items-center gap-2">
+          <CiSettings />
+          Settings
+          </span>
+        </NavLink>
       </nav>
     </div>
   );
