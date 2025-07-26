d4639f109c9f24ca4fbc3faf963beb2361bf53e9
Yusuf Ali <isaihmed09@gmail.com>
2025-07-26 22:25:01 +0600

    add propertys button added

--- frontend/src/pages/Propertys.tsx
+++ frontend/src/pages/Propertys.tsx
@@ -1,6 +1,17 @@
+import { Link } from "react-router-dom"
+
 function Propertys() {
   return (
-    <div>Property</div>
+    <div>
+      <div className="flex justify-between">
+        <h5 className="text-white font-semibold">Propertyes</h5>
+        <Link 
+        className="text-gray-800 font-semibold bg-white px-2 py-1 rounded-sm cursor-pointer hover:bg-gray-300 transition-colors ease-in-out duration-200"
+        to={'/propertys/add-property'} >
+          Add Property
+        </Link>
+      </div>
+    </div>
   )
 }
 
