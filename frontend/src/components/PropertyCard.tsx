bb52fc3ae608f672599442a334ba19a0e7c141ad
Yusuf Ali <isaihmed09@gmail.com>
2025-08-02 11:49:38 +0600

    Delete Property Functionnality done

--- frontend/src/components/PropertyCard.tsx
+++ frontend/src/components/PropertyCard.tsx
@@ -1,32 +1,79 @@
 import { useNavigate } from "react-router-dom";
 import { type property } from "../lib/utils";
+import { FaDeleteLeft } from "react-icons/fa6";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
 
-function PropertyCard({ propertyItem }: { propertyItem: property }) {    
-    const navigate = useNavigate();
+function PropertyCard({ propertyItem }: { propertyItem: property }) {
+  const notify = (message: string) => toast(message);
 
-    const handelCardClidk = () => {
-        navigate(`/propertys/${propertyItem._id}`)
-    }
-
-    return (
-        <div className="max-h-48 flex items-center gap-6 bg-[#21222D] p-2 rounded-md cursor-pointer"
-        onClick={handelCardClidk}
-        >
-            <img className="w-48 rounded-md" src={propertyItem.images[0]} alt={propertyItem.title} />
-            <div className="flex flex-col gap-1">
-                <h5 className="text-xl font-medium">{propertyItem.title}</h5>
-                <div className="flex items-center gap-2">
-                    <p className="text-lg font-normal">{propertyItem.bathrooms} bedrooms</p>
-                    <p className="text-lg font-normal">{propertyItem.bathrooms} bathrooms</p>
-                    <p className="text-lg font-normal">{propertyItem.size} </p>
-                </div>
-                <div className="flex items-center gap-2">
-                    <p className="text-lg font-normal">{propertyItem.price}</p>
-                    <p className="text-lg font-normal">{propertyItem.status}</p>
-                </div>
-            </div>
-        </div>
+  const deleteProperty = async () => {
+    axios.defaults.withCredentials = true;
+    const response = await axios.delete(
+      `http://localhost:3000/api/v1/property/delete-property/${propertyItem._id}`
     );
+    return response.data;
+  };
+
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: deleteProperty,
+    onSuccess: () => {
+      notify("Successfully Property Deleted");
+      // Optionally refetch or update query cache
+      queryClient.invalidateQueries({ queryKey: ["propertys"] });
+    },
+    onError: () => {
+      notify("Property could not be deleted");
+    },
+  });
+
+  const navigate = useNavigate();
+
+  const handelCardClidk = () => {
+    navigate(`/propertys/${propertyItem._id}`);
+  };
+
+  return (
+    <div className="max-h-48 flex items-center gap-6 bg-[#21222D] p-2 rounded-md relative">
+      <ToastContainer />
+      <img
+        className="w-48 rounded-md cursor-pointer hover:scale-105 transition-all ease duration-100"
+        src={propertyItem.images[0]}
+        alt={propertyItem.title}
+        onClick={handelCardClidk}
+      />
+      <div className="flex flex-col gap-1">
+        <h5
+          className="text-xl font-medium cursor-pointer hover:text-blue-300  hover:underline"
+          onClick={handelCardClidk}
+        >
+          {propertyItem.title}
+        </h5>
+        <div className="flex items-center gap-2">
+          <p className="text-lg font-normal">
+            {propertyItem.bathrooms} bedrooms
+          </p>
+          <p className="text-lg font-normal">
+            {propertyItem.bathrooms} bathrooms
+          </p>
+          <p className="text-lg font-normal">{propertyItem.size} </p>
+        </div>
+        <div className="flex items-center gap-2">
+          <p className="text-lg font-normal">{propertyItem.price}</p>
+          <p className="text-lg font-normal">{propertyItem.status}</p>
+        </div>
+      </div>
+      <span
+        className="absolute bottom-2 right-2 cursor-pointer hover:text-red-400 text-2xl"
+        onClick={() => mutate()}
+      >
+        <FaDeleteLeft />
+      </span>
+    </div>
+  );
 }
 
-export default PropertyCard
+export default PropertyCard;
